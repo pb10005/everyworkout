23248597ac907428025c408a01d23efba1691b55
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2023-03-09 16:02:52 +0000

    Update schema.prisma and muscle.ts

--- src/server/api/routers/muscle.ts
+++ src/server/api/routers/muscle.ts
@@ -5,5 +5,5 @@ import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 export const muscleRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.muscle.findMany();
-  }),
+  })
 });

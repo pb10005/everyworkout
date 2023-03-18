3ca1f218e087cfcccdc9c70b026eac17657adb1c
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2023-03-18 11:46:57 +0000

    Update MaximumCard.tsx

--- src/components/MaximumCard.tsx
+++ src/components/MaximumCard.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Link from "next/link";
 
 type Props = {
-  date: Date | undefined;
+  date?: Date | undefined;
   exerciseName: string | undefined;
   metrics_code: string;
   value: number | null;

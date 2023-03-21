ba0b361d6b6b3dc27a65170ff2817515cacdc9eb
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2023-03-21 09:50:23 +0000

    Update layout

--- src/components/MaximumCard.tsx
+++ src/components/MaximumCard.tsx
@@ -21,7 +21,7 @@ export const MaximumCard: React.FC<Props> = (props: Props) => {
 
   return (
     <>
-      <div className="flex justify-between rounded-lg bg-gradient-to-r from-amber-400 via-amber-100 to-amber-500 p-2 shadow shadow-amber-500/50">
+      <div className="w-full flex justify-between rounded-lg bg-gradient-to-r from-amber-400 via-amber-100 to-amber-500 p-2 shadow shadow-amber-500/50">
         <section>
           <p className="text-lg font-bold">
             <span>{exerciseName}</span>

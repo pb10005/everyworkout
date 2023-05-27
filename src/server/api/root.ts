7017f6c6b329bfe9b0828f6e10d6a467dcf8490b
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2023-05-27 06:15:47 +0000

    Add profile page

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -3,6 +3,7 @@ import { muscleRouter } from "./routers/muscle";
 import { exerciseRouter } from "./routers/exercise";
 import { workoutRouter } from "./routers/workout";
 import { maximumRouter } from "./routers/maximum";
+import { profileRouter } from "./routers/profile";
 
 /**
  * This is the primary router for your server.
@@ -14,6 +15,7 @@ export const appRouter = createTRPCRouter({
   exercise: exerciseRouter,
   workout: workoutRouter,
   maximum: maximumRouter,
+  profile: profileRouter,
 });
 
 // export type definition of API

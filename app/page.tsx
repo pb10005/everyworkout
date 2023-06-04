40e000c2e8046421a187873eedb4aa40661244eb
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2023-06-04 02:51:31 +0000

    Update lp

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,22 @@
 
 import { type NextPage } from "next";
 import Link from "next/link";
+import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
   return (
     <>
-      <main id="lp" className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main id="lp" className="flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Every <span className="text-[hsl(280,100%,70%)]">W</span>orkout
+          <Image width={500} height={500} src="/assets/Weights-rafiki.svg" alt="Weights"></Image>
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Every <span className="text-cyan-700">W</span>orkout
           </h1>
           <div className="flex flex-col items-center gap-2">
             <AuthShowcase />
           </div>
+          <a href="https://storyset.com/people">People illustrations by Storyset</a>
         </div>
       </main>
     </>
@@ -28,18 +31,18 @@ const AuthShowcase: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-white">
+      <p className="text-center">
         {sessionData && (
           <Link
             href="/dashboard"
-            className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+            className="rounded-full px-10 py-3 font-semibold no-underline shadow"
           >
             Dashboard
           </Link>
         )}
       </p>
       <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        className="rounded-full px-10 py-3 font-semibold no-underline shadow"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
         {sessionData ? "Sign out" : "Sign in"}

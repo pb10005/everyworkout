efaca6426c2ba2c146416ed3387200f4c3a7deeb
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2023-07-30 23:18:07 +0900

    Update layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { AuthShowcase } from "../src/components";
 const Home: NextPage = () => {
   return (
     <>
-      <main id="lp" className="flex min-h-screen flex-col items-center justify-center">
+      <main id="lp" className="flex min-h-screen flex-col items-center justify-center bg-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <Image width={500} height={500} src="/assets/Weights-rafiki.svg" alt="Weights"></Image>
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 export const Heading: React.FC = () => {
     return (
         <>
-            <div className="fixed top-0 py-2 bg-cyan-700 flex justify-center items-center w-full">
+            <div className="fixed top-0 py-2 bg-white flex justify-center items-center w-full">
                 <Link href="/">
-                    <span className="text-xl text-white font-bold">EveryWorkout</span>
+                    <span className="text-xl text-cyan-800 tracking-widest font-bold">EVERYWORKOUT</span>
                 </Link>
             </div>
         </>

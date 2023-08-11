5d46c2dc1d7dc181de49327dc2d518ee73baf98b
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2023-08-11 17:24:04 +0900

    Update layout

--- src/components/RecordCard.tsx
+++ src/components/RecordCard.tsx
@@ -30,32 +30,36 @@ export const RecordCard: React.FC<Props> = (props: Props) => {
   const dateDisplay = date.toISOString().split("T")[0];
   return (
     <>
-      <div className="mb-2 rounded-lg p-2 shadow-lg bg-white">
-        <p className="font-bold">
-          <Link href={`/workout/${id}`}>{exerciseName}</Link>
-        </p>
-        <div className="text-sm text-gray-500">{dateDisplay}</div>
-        <div className="flex justify-center items-center gap-2 divide-x">
-          <span className="w-20 flex flex-col items-center">
-            <span className="text-2xl font-extrabold">{weight}</span>
-            <span className="text-gray-500 text-xs">kg</span>
-          </span>
-          <span className="w-20 flex flex-col items-center">
-            <span className="text-2xl font-extrabold">{reps}</span>
-            <span className="text-gray-500 text-xs">reps</span>
-          </span>
-          <span className="w-20 flex flex-col items-center">
-            <span className="text-2xl font-extrabold">{sets}</span>
-            <span className="text-gray-500 text-xs">sets</span>
-          </span>
+      <div className="grid cols-12 rounded-lg p-2 shadow-lg bg-white">
+        <div className="sm:col-span-10 sm:col-start-2">
+          <p className="font-bold">
+            <Link href={`/workout/${id}`}>{exerciseName}</Link>
+          </p>
+          <div className="text-sm text-gray-500">{dateDisplay}</div>
+          {muscles.map(m => {
+            return (
+              <div key={m.id} className="flex gap-1">
+                <span className="inline-block text-sm bg-gray-100 rounded-lg p-2">{m.name}</span>
+              </div>
+            )
+          })}
         </div>
-        {muscles.map(m => {
-          return (
-            <div key={m.id} className="flex gap-1">
-              <span className="inline-block text-sm bg-gray-100 rounded-lg p-2">{m.name}</span>
-            </div>
-          )
-        })}
+        <div className="col-span-12 my-3">
+          <div className="flex justify-center items-center gap-2 divide-x">
+            <span className="w-20 flex flex-col items-center">
+              <span className="text-2xl font-extrabold text-cyan-700">{weight}</span>
+              <span className="text-gray-500 text-xs">kg</span>
+            </span>
+            <span className="w-20 flex flex-col items-center">
+              <span className="text-2xl font-extrabold text-cyan-700">{reps}</span>
+              <span className="text-gray-500 text-xs">reps</span>
+            </span>
+            <span className="w-20 flex flex-col items-center">
+              <span className="text-2xl font-extrabold text-cyan-700">{sets}</span>
+              <span className="text-gray-500 text-xs">sets</span>
+            </span>
+          </div>
+        </div>
       </div>
     </>
   );

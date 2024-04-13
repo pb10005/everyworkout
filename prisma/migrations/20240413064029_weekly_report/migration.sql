5377db782844df8b9c6cf71308d5f8e2f999b6ad
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2024-04-13 16:05:10 +0900

    Migration

---
+++ prisma/migrations/20240413064029_weekly_report/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Added the required column `weeklyReportPublished` to the `Workout` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Workout" ADD COLUMN     "weeklyReportPublished" BOOLEAN NOT NULL;
+
+-- CreateTable
+CREATE TABLE "WeeklyReport" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "WeeklyReport_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "WeeklyReport" ADD CONSTRAINT "WeeklyReport_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

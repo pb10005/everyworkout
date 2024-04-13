539c29a6699315f87e53c042440e9a6f56b2d69c
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2024-04-13 15:41:24 +0900

    Implement report

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -5,6 +5,9 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
+  if (request.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`) {
+    return response.status(401).end('Unauthorized');
+  }
   const data = await generateReport();
   response.status(200).json({ success: true, data: data });
 }
--- src/utils/report.ts
+++ src/utils/report.ts
@@ -1,21 +1,52 @@
 import { prisma } from "../server/db";
 
-type Report = {
-    name: string;
-    count: number;
-};
-
 export const generateReport = async () => {
+    const dateLt = new Date();
+    
+    const dateGte = new Date(dateLt);
+    dateGte.setDate(dateGte.getDate() - 7);
+
     const users = await prisma.user.findMany();
     const exercises = await prisma.exercise.findMany();
+
     const result = await prisma.workout.groupBy({
-        by: ["userId", "exerciseId"],
-        _count: true
+        by: ["userId"],
+        _count: true,
+        where: {
+            date: {
+              gte: dateGte,
+              lt:  dateLt
+            },
+            weeklyReportPublished: false
+        }
     });
 
-    return result.map(r => {
+    const reports = result.map(r => {
         const name = users.find(u => u.id === r.userId)?.name;
-        const exerciseName = exercises.find(e => e.id === r.exerciseId)?.name;
-        return { name, exerciseName, count: r._count };
+        const report = `${name}さんは${r._count}回ワークアウトしました`;
+
+        return {
+            userId: r.userId || '',
+            content: report
+        }
     });
+
+    await prisma.weeklyReport.createMany({
+        data: reports
+    });
+
+    await prisma.workout.updateMany({
+        data: {
+            weeklyReportPublished: true
+        },
+        where:  {
+            date: {
+              gte: dateGte,
+              lt:  dateLt
+            },
+            weeklyReportPublished: false
+        }
+    });
+
+    return 0;
 };

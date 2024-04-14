011016d5b5547eda73be13ed01cfe57fff61115c
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2024-04-14 15:57:30 +0900

    バッチのトランザクション

--- src/utils/report.ts
+++ src/utils/report.ts
@@ -1,52 +1,56 @@
 import { prisma } from "../server/db";
 
 export const generateReport = async () => {
-    const dateLt = new Date();
+    await prisma.$transaction(async (prisma) => {
+        const dateLt = new Date();
+        
+        const dateGte = new Date(dateLt);
+        dateGte.setDate(dateGte.getDate() - 7);
     
-    const dateGte = new Date(dateLt);
-    dateGte.setDate(dateGte.getDate() - 7);
-
-    const users = await prisma.user.findMany();
-    const exercises = await prisma.exercise.findMany();
-
-    const result = await prisma.workout.groupBy({
-        by: ["userId"],
-        _count: true,
-        where: {
-            date: {
-              gte: dateGte,
-              lt:  dateLt
+        const users = await prisma.user.findMany();
+        const exercises = await prisma.exercise.findMany();
+    
+        const result = await prisma.workout.groupBy({
+            by: ["userId"],
+            _count: true,
+            where: {
+                date: {
+                  gte: dateGte,
+                  lt:  dateLt
+                },
+                weeklyReportPublished: false
+            }
+        });
+    
+        const reports = result.map(r => {
+            const name = users.find(u => u.id === r.userId)?.name;
+            const report = `${name || 'Anonymous'}さんは先週${r._count}回ワークアウトしました！`;
+    
+            return {
+                userId: r.userId || '',
+                content: report
+            }
+        });
+    
+        // レポートを登録する
+        await prisma.weeklyReport.createMany({
+            data: reports
+        });
+    
+        // レポート作成済みフラグを立てる
+        await prisma.workout.updateMany({
+            data: {
+                weeklyReportPublished: true
             },
-            weeklyReportPublished: false
-        }
+            where:  {
+                date: {
+                  gte: dateGte,
+                  lt:  dateLt
+                },
+                weeklyReportPublished: false
+            }
+        });
     });
-
-    const reports = result.map(r => {
-        const name = users.find(u => u.id === r.userId)?.name;
-        const report = `${name || 'Anonymous'}さんは${r._count}回ワークアウトしました`;
-
-        return {
-            userId: r.userId || '',
-            content: report
-        }
-    });
-
-    await prisma.weeklyReport.createMany({
-        data: reports
-    });
-
-    await prisma.workout.updateMany({
-        data: {
-            weeklyReportPublished: true
-        },
-        where:  {
-            date: {
-              gte: dateGte,
-              lt:  dateLt
-            },
-            weeklyReportPublished: false
-        }
-    });
-
+    
     return 0;
 };

806cabe1623e45f399cf3482ff769960f44fc037
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2024-04-21 16:41:50 +0900

    localStorageが見つからない

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -3,27 +3,37 @@ import { useState, useEffect } from "react";
 
 export const useDarkMode = () => {
     const [darkMode, setDarkMode] = useState((): string => {
-        return window.localStorage.getItem('darkMode') || '';
+        try {
+            return window.localStorage.getItem('darkMode') || '';
+        } catch {
+            return '';
+        }
     });
 
     const toggleDarkMode = (): void => {
-        const darkMode = window.localStorage.getItem('darkMode') || '';
-        if (darkMode === 'dark') {
-            setDarkMode('');
-        }
-        else {
-            setDarkMode('dark');
-        }
+        try {
+            const darkMode = window.localStorage.getItem('darkMode') || '';
+            if (darkMode === 'dark') {
+                setDarkMode('');
+            }
+            else {
+                setDarkMode('dark');
+            }
+        } catch {}
     };
 
     // stateとlocalStorageを同期する
     useEffect(() => {
-        window.localStorage.setItem('darkMode', darkMode);
-        if (darkMode === 'dark') {
-            document.documentElement.classList.remove('dark')
-        }
-        else {
-            document.documentElement.classList.add('dark')
+        try {
+            window.localStorage.setItem('darkMode', darkMode);
+            if (darkMode === 'dark') {
+                document.documentElement.classList.remove('dark')
+            }
+            else {
+                document.documentElement.classList.add('dark')
+            }
+        } catch {
+
         }
 
     }, [darkMode, setDarkMode]);

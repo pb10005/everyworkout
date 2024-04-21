86ebd07e5b35ea7a2d378e2cb1cd61d9a6880a52
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2024-04-21 16:46:32 +0900

    Profileのダークモード対応

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -14,17 +14,21 @@ const Profile: NextPage = () => {
 
   return (
     <>
-      <main className="">
+      <main className="mt-4">
         <Heading />
         <Navigation />
         <div className="grid md:grid-cols-12">
-          <div className="md:col-span-6 md:col-start-4">
+          <div className="md:col-span-6 md:col-start-4 rounded-lg dark:outline outline-1 outline-gray-500 p-2">
             <section className="mb-2 p-2">
               {isSuccess && <>
-                <p className="text-sm text-gray-500">ログインユーザー(公開されません)</p>
-                {data?.image && <img src={data.image}></img>}
-                <div className="text-lg font-bold">{data?.name}</div>
-                <div>{data?.email}</div>
+                <p className="text-sm text-gray-700 dark:text-gray-300">ログインユーザー(公開されません)</p>
+                <div className="flex items-center gap-2">
+                  {data?.image && <img src={data.image} width={60}></img>}
+                  <div className="flex flex-col gap-1">
+                    <div className="text-lg font-bold dark:text-white">{data?.name}</div>
+                    <div className="dark:text-white">{data?.email}</div>
+                  </div>
+                </div>
               </>}
             </section>
             <Button onClick={() => toggleDarkMode()}>ダークモード切り替え</Button>

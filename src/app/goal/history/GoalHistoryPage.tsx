2305cea84e4d0c701aea04e9d149a1658ad89d53
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2024-05-19 16:23:53 +0900

    過去の目標を修正できるようにする

--- src/app/goal/history/GoalHistoryPage.tsx
+++ src/app/goal/history/GoalHistoryPage.tsx
@@ -1,19 +1,30 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import React from "react";
-import { ListContainer, Subheader } from "../../../components";
+import { Dropdown, DropdownItem, ListContainer, Subheader } from "../../../components";
 import { api } from "../../../utils/api";
 
 export const GoalHistoryPage: React.FC = () => {
     const { data: goals } = api.goal.getUserGoals.useQuery({});
+    const router = useRouter();
 
     return (<>
         <Subheader content="過去の目標" />
         <ListContainer>
             {goals && goals.map(g => <>
-                <div key={g.id} className="dark:text-white px-4 py-2 flex justify-between">
-                    <span>{g.content}</span>
-                    <span>{g.updatedAt.toISOString().split('T')[0] || ''}</span>
+                <div key={g.id} className="dark:text-white p-2 flex justify-between items-center">
+                    <div className="whitespace-pre-wrap">{g.content}</div>
+                    <div className="flex gap-1 items-center">
+                        <span>{g.updatedAt.toISOString().split('T')[0] || ''}</span>
+                        <Dropdown>
+                            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
+                                <DropdownItem onClick={() => router.push(`/goal/edit/${g.id}`)}>
+                                    編集
+                                </DropdownItem>
+                            </ul>
+                        </Dropdown>
+                    </div>
                 </div>
             </>)}
         </ListContainer>
